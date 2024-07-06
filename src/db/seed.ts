dfa2a9be402a7c5a0f6b3da36ab07984609094a4
m4c1elz <milaelipe.maciel@gmail.com>
2024-07-06 19:21:15 -0300

    ci(seed): add comment and post seeding

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -10,22 +10,72 @@ async function seed() {
     spinner.start({
         text: "Gerando usuários...",
     })
-    await db.insert(users).values(
-        Array.from({ length: 20 }).map(() => ({
-            name: faker.person.firstName(),
-            email: faker.internet.email(),
-            password: hashSync(faker.internet.password(), 10),
-            atsign: faker.person.firstName().toLowerCase(),
-            description: faker.person.bio(),
-            isVerified: 1,
-            followers: faker.number.int({
-                min: 10,
-                max: 100,
-            }),
-        }))
+
+    let availableUsers: number[] = []
+    let availablePosts: number[] = []
+
+    await Promise.all(
+        Array.from({ length: 20 }).map(async () => {
+            try {
+                const [user] = await db.insert(users).values({
+                    name: faker.person.firstName(),
+                    email: faker.internet.email(),
+                    password: hashSync(faker.internet.password(), 10),
+                    atsign: faker.person.firstName().toLowerCase(),
+                    description: faker.person.bio(),
+                    isVerified: 1,
+                    followers: faker.number.int({
+                        min: 10,
+                        max: 100,
+                    }),
+                    createdAt: faker.date
+                        .between({
+                            from: new Date("2024-01-01"),
+                            to: new Date(),
+                        })
+                        .toLocaleDateString("en-ca"),
+                })
+                availableUsers.push(user.insertId)
+            } catch (error) {
+                console.log(error)
+            }
+        })
     )
+
+    spinner.update({
+        text: "Adicionando postagens...",
+    })
+
+    await Promise.all(
+        availableUsers.map(async user => {
+            const [post] = await db.insert(posts).values({
+                userId: user,
+                post: faker.person.bio(),
+            })
+            availablePosts.push(post.insertId)
+        })
+    )
+
+    spinner.update({
+        text: "Adicionando comentários...",
+    })
+
+    await Promise.all(
+        availablePosts.map(async post => {
+            await db.insert(comments).values({
+                postId: availablePosts[
+                    Math.floor(Math.random() * availableUsers.length)
+                ],
+                userId: availableUsers[
+                    Math.floor(Math.random() * availableUsers.length)
+                ],
+                comment: faker.person.bio(),
+            })
+        })
+    )
+
     spinner.success({
-        text: "Usuários gerados!",
+        text: "Seed do banco de dados feito com sucesso.",
     })
 
     process.exit()
