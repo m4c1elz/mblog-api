52b193c7f48cb17b6a91d94f3b873fdad9c9ca8a
m4c1elz <milaelipe.maciel@gmail.com>
2024-07-07 21:44:05 -0300

    fix: make logout route require access token

--- src/routes/auth-routes.ts
+++ src/routes/auth-routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express"
 import { authController } from "../controllers/auth-controller"
+import { validateToken } from "../middlewares/validate-token"
 
 const router = Router()
 
 router.get("/refresh", authController.refresh)
 router.get("/confirm/:token", authController.verifyEmail)
-router.get("/logout", authController.logout)
+router.get("/logout", validateToken, authController.logout)
 router.post("/login", authController.login)
 router.post("/register", authController.register)
 
