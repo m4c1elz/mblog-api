d2635e1f13a5de48b21e3918e34e782933d35cd8
m4c1elz <milaelipe.maciel@gmail.com>
2024-07-16 12:16:22 -0300

    chore: removed email logging

--- src/mail/mail.ts
+++ src/mail/mail.ts
@@ -19,22 +19,18 @@ export function sendConfirmationEmail({
     to: string
     token: string
 }) {
-    transport
-        .sendMail({
-            from: API_MAIL_USER,
-            to,
-            subject: "Verificação de email",
-            html: render(
-                Email({
-                    email: to,
-                    authLink: new URL(
-                        `/verify/${token}`,
-                        EMAIL_REDIRECT_BASE_URL
-                    ).toString(),
-                })
-            ),
-        })
-        .then(emailInfo => {
-            console.log(emailInfo.envelope)
-        })
+    transport.sendMail({
+        from: API_MAIL_USER,
+        to,
+        subject: "Verificação de email",
+        html: render(
+            Email({
+                email: to,
+                authLink: new URL(
+                    `/verify/${token}`,
+                    EMAIL_REDIRECT_BASE_URL
+                ).toString(),
+            })
+        ),
+    })
 }
